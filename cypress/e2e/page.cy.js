e1df3dd6ec41f9c0de18225e4760ff4e190b78c2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-10-27 12:48:28 -0400

    a2

--- cypress/e2e/page.cy.js
+++ cypress/e2e/page.cy.js
@@ -6,7 +6,19 @@ describe('Calculator', () => {
   it('loads', () => {
     // visit the HTML page (could be a simple file)
     // https://on.cypress.io/visit
+    cy.visit('public/index.html')
     // confirm the page title is "TDD Calculator"
     // https://on.cypress.io/title
+    cy.title().should('equal', 'TDD Calculator')
+  })
+
+  it('has the expected elements', () => {
+    cy.visit('public/index.html')
+    // confirm there is an element with id "calculator"
+    // and inside there are elements with id "display" and "buttons"
+    // https://on.cypress.io/get
+    // https://on.cypress.io/find
+    // https://on.cypress.io/within
+    // one solution
   })
 })
