270a5229fbad0bb83a45ceb1912fdf1c7ce6fd31
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-10-28 08:18:55 -0400

    initial lesson a4

---
+++ cypress/e2e/buttons.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+describe('Calculator', () => {
+  it('has all necessary buttons', () => {
+    cy.visit('public/index.html')
+    // these are the button labels we expect to have
+    // arranged in a grid (to be implemented later)
+    const labels = [
+      // first row
+      '+',
+      '7',
+      '8',
+      '9',
+      // second row
+      '-',
+      '5',
+      '4',
+      '6',
+      // third row
+      '*',
+      '1',
+      '2',
+      '3',
+      // fourth row
+      '/',
+      '0',
+      '.',
+      '=',
+      // clear button
+      'C',
+    ]
+    // confirm there is the correct number of buttons
+    // https://on.cypress.io/get
+    // https://on.cypress.io/should
+    //
+    // check that there is a button element with the matching label
+    // https://on.cypress.io/contains
+  })
+})
--- cypress/e2e/network-resources.cy.js
+++ cypress/e2e/network-resources.cy.js
@@ -6,19 +6,24 @@ describe('Calculator', () => {
     // give the spy an alias "styles"
     // https://on.cypress.io/intercept
     // https://on.cypress.io/as
-    //
+    cy.intercept('styles.css').as('styles')
     // set up a network spy on "app.js"
     // give the spy an alias "app"
-    //
+    cy.intercept('app.js').as('app')
     cy.visit('public/index.html')
     // wait for the "styles.css" to be loaded
     // https://on.cypress.io/wait
     // confirm the server response code is either 200 or 304
     // https://on.cypress.io/its
     // https://glebbahmutov.com/cypress-examples/commands/assertions.html
-    //
+    cy.wait('@styles')
+      .its('response.statusCode')
+      .should('be.oneOf', [200, 304])
     // confirm the application javascript loads
     // by waiting on the "app.js" resource
     // and confirming its response status code
+    cy.wait('@app')
+      .its('response.statusCode')
+      .should('be.oneOf', [200, 304])
   })
 })
