bb1ccc6bdcba76e0c15ce69880c5f1f6e26a2fa2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-10-28 18:52:21 -0400

    add starting code

--- cypress/e2e/decimals.cy.js
+++ cypress/e2e/decimals.cy.js
@@ -3,17 +3,47 @@ describe('Calculator', () => {
     cy.visit('public/index.html')
   })
 
-  // for each test, come up with an expression
-  // and then enter it into the calculator
-  // confirm the display shows the correct result
+  it('enters whole numbers', () => {
+    const expression = '123'
+    cy.enterExpression(expression)
+    cy.get('#display').should('have.text', expression)
+  })
 
-  it('enters whole numbers')
+  it('enters a floating-point number', () => {
+    const expression = '123.45'
+    cy.enterExpression(expression)
+    cy.get('#display').should('have.text', expression)
+  })
 
-  it('enters a floating-point number')
+  it('enters a floating point number between 0 and 1', () => {
+    const expression = '0.123'
+    cy.enterExpression(expression)
+    cy.get('#display').should('have.text', expression)
+  })
 
-  it('enters a floating point number between 0 and 1')
+  it('enters a negative floating point number', () => {
+    const expression = '-9.876'
+    cy.enterExpression(expression)
+    cy.get('#display').should('have.text', expression)
+  })
 
-  it('enters a negative floating point number')
+  it('enters a sum of two floating point numbers', () => {
+    const expression = '1.1+2.2'
+    cy.enterExpression(expression)
+    cy.get('#display').should('have.text', '1.1+2.2')
+  })
 
-  it('enters a sum of two floating point numbers')
+  context('edge-cases', () => {
+    // should skip the second dot: "1.1.1" => "1.11"
+    it(
+      'cannot enter a floating-point number with multiple decimal points',
+    )
+
+    // should add zero automatically: ".12" => "0.12"
+    it('cannot start a number with a dot')
+
+    // should add zero automatically to the second number
+    // "1+.23" => "1+0.23"
+    it('cannot continue an expression with a dot')
+  })
 })
