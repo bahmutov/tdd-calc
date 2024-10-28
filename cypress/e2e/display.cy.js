7211edf38c7fd33206c289b5cf601cfc5add34b4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-10-28 09:12:55 -0400

    starting code for a6

--- cypress/e2e/display.cy.js
+++ cypress/e2e/display.cy.js
@@ -6,10 +6,24 @@ describe('Calculator', () => {
     // type number "123" by clicking individual digits
     // https://on.cypress.io/contains
     // https://on.cypress.io/click
-    //
+    cy.contains('button', '1').click()
+    cy.contains('button', '2').click()
+    cy.contains('button', '3').click()
     // confirm the display element has the text "123"
     // https://on.cypress.io/contins
     // or you can get the element and check its text
     // https://on.cypress.io/should
+    cy.get('#display').should('have.text', '123')
+  })
+
+  it('shows entered expression', () => {
+    cy.visit('public/index.html')
+    // type expression "123+456"
+    // by clicking individual digits and operators
+    const expression = '123+456'
+    // split the expression into individual characters
+    // and click the corresponding button
+    //
+    // confirm the display shows the whole expression
   })
 })
--- public/app.js
+++ public/app.js
@@ -2,6 +2,9 @@
 
 /**
  * Function that receives a digit to append to the currently displayed text
- * @param {number} digit A single digit to append to the display text
+ * @param {number|'+'|'-'|'*'|'/'} digit A single digit to append to the display text
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+  const display = document.getElementById('display')
+  display.innerText += digit
+}
