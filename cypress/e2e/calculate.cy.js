6c2f2b17b894ce23cd63f128c060c7a38ecc920d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-10-28 22:13:32 -0400

    c2 initial code

--- cypress/e2e/calculate.cy.js
+++ cypress/e2e/calculate.cy.js
@@ -6,9 +6,17 @@ describe('Calculator', () => {
     // click the "=" button
     // https://on.cypress.io/contains
     // https://on.cypress.io/click
-    //
+    cy.contains('#buttons button', '=').click()
     // display should have text "3"
     // https://on.cypress.io/get
     // https://on.cypress.io/should
+    cy.get('#display').should('have.text', '3')
+  })
+
+  it('shows an error for invalid expressions', () => {
+    cy.visit('public/index.html')
+    // enter an invalid numerical expression
+    // like "1++2" and press "="
+    // confirm the display shows "ERROR"
   })
 })
