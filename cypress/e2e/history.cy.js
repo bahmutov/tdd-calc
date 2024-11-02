28668b956dca4e3f99b4c97c24f21a067401878b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-11-01 22:19:10 -0400

    prepare d6

--- cypress/e2e/history.cy.js
+++ cypress/e2e/history.cy.js
@@ -3,12 +3,15 @@ describe('History', () => {
     cy.visit('public/index.html')
     // set the current viewport to "iphone-6"
     // https://on.cypress.io/viewport
-    //
+    cy.viewport('iphone-6')
     // confirm the #history element is not visible
+    cy.get('#history').should('not.be.visible')
     // confirm the #right-column element has display: none
-    //
+    cy.get('#right-column').should('have.css', 'display', 'none')
     // set the current viewport to "macbook-15"
     // https://on.cypress.io/viewport
+    cy.viewport('macbook-15')
     // confirm the #history element is visible
+    cy.get('#history').should('be.visible')
   })
 })
