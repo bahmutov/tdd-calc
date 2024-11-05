b4d54f753005f4c55f0d35227c19b20048e9e2d7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-11-05 13:28:18 -0500

    prepare for e4

--- cypress/e2e/append-dot.cy.js
+++ cypress/e2e/append-dot.cy.js
@@ -1,26 +1,8 @@
 // we can test complex logic written in a single function
 // by testing it in isolation without going through the UI
+import { appendDot } from '../../public/utils'
+
 describe('Calculator appendDot', () => {
-  // store the function in a variable
-  // so we can use it in each test
-  let appendDot
-
-  before(() => {
-    // visit the page, which loads the JavaScript
-    // and makes all functions available on the "window" object
-    // Tip: cy.visit yields the "window" object
-    // so you can grab its property "appendDot"
-    // and store it in the variable
-    // https://on.cypress.io/visit
-    // https://on.cypress.io/its
-    // https://on.cypress.io/then
-    cy.visit('public/index.html')
-      .its('appendDot')
-      .then((fn) => {
-        appendDot = fn
-      })
-  })
-
   // in each test you can call the "appendDot" function
   // and use Chai assertions to check the result
   // https://glebbahmutov.com/cypress-examples/commands/assertions.html
--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 // calculator logic
 
+import { appendDot } from './utils.js'
+
 // the current list of history entries
 const history = []
 
@@ -53,30 +55,6 @@ try {
 // we probably want to keep around the reference to the
 // LI element with ID "history-list" to append new history items
 const historyListElement = document.getElementById('history-list')
-
-export function appendDot(expression) {
-  // check if we are trying a number right now
-  // this will split expressions like
-  // "1+2.3" into ["1", "2.3"]
-  const parts = expression.split(/[\+\-\*\/]/)
-  // the last part is the current number
-  // the user is appending to
-  const last = parts[parts.length - 1]
-  //
-  // if the last part already has a dot, do nothing
-  if (last.includes('.')) {
-    return expression
-  }
-  // if the last part is empty, add a zero before the dot
-  // this will cover starting to type a number with a dot
-  // or typing a dot after an expression like "1+"
-
-  if (last === '') {
-    return expression + '0.'
-  }
-  // otherwise, add the dot and return the expression
-  return expression + '.'
-}
 
 /**
  * Function that receives a digit to append to the currently displayed text
