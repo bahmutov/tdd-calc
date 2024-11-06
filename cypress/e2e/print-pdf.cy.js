14cbd2450338d91ab870d3374bfc680b5c41c451
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-11-06 14:03:32 -0500

    prepare e6

--- cypress/e2e/print-pdf.cy.js
+++ cypress/e2e/print-pdf.cy.js
@@ -12,28 +12,40 @@ it(
     cy.get('#calculator')
       .should('be.visible')
 
-      // before printing the browser window into PDF
-      // remove the Cypress UI leaving just the app contents
       .then(() => {
+        // before printing the browser window into PDF
+        // remove the Cypress UI leaving just the app contents
         window.top.document
           .querySelector('#spec-runner-header')
           .remove()
         window.top.document
-          .querySelector('[data-cy="sidebar"]')
-          .remove()
-        window.top.document
           .querySelector('[data-cy="reporter-panel"]')
           .remove()
+
+        // only if we are using "cypress open"
+        // it shows the specs widget
+        if (Cypress.browser.isHeaded) {
+          window.top.document
+            .querySelector('[data-cy="sidebar"]')
+            .remove()
+        }
       })
 
     cy.CDP('Page.enable')
     // https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-printToPDF
     // use landscape mode and print the background, scale 0.75
     // return the PDF as base64
-    //
-    // which will be an object { data: 'base64 string' }
-    // grab the "data" property and write it to a file
-    // https://on.cypress.io/its
-    // https://on.cypress.io/writefile
+    cy.CDP('Page.printToPDF', {
+      landscape: true,
+      printBackground: true,
+      scale: 0.75,
+      transferMode: 'ReturnAsBase64',
+    })
+      // which will be an object { data: 'base64 string' }
+      // grab the "data" property and write it to a file
+      // https://on.cypress.io/its
+      // https://on.cypress.io/writefile
+      .its('data')
+      .then((data) => cy.writeFile('calculator.pdf', data, 'base64'))
   },
 )
